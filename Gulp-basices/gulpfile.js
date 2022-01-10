fb7035fd5b1557aa0a9a3626e9a134550b11749a
montasserbadry <muntasser.badry94@yahoo.com>
2022-01-10 11:37:00 +0200

    starter react project

---
+++ Gulp-basices/gulpfile.js
@@ -0,0 +1,20 @@
+
+const gulp = require('gulp');
+const gulpSass = require('gulp-sass');
+const sass = require('sass');
+
+
+gulp.task('log' , function(){
+    console.log('login')
+})
+gulp.task('move' ,async function() {
+    gulp.src('css1/*.css').pipe(gulp.dest('css2'))
+})
+gulp.task('watching', function() {
+    gulp.watch('css1/style.css', async function() {
+        gulp.src('css1/*.css').pipe(gulp.dest('css2'))
+    })
+})
+gulp.task('sass',async function() {
+    gulp.src('index.scss').pipe(sass()).pipe(gulp.dest('css'))
+})
